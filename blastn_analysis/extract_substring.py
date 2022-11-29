e7307d69f6028fc20b6960d89b4a9cfc4f2ef0a7
Kaiyuan Zhu <kzhu@notifier@genomemaster.ucsd.edu>
2022-11-28 23:20:36 -0800

    Updated blastn analysis procedure.

--- blastn_analysis/extract_substring.py
+++ blastn_analysis/extract_substring.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 	chr_string = ""
 	chr = ""
 	i = 0
-	fp = open("/ribosome/projects/Nanopore/hg38full.fa", 'r')
+	fp = open(sys.argv[1], 'r')
 	for line in fp:
 		s = line.strip()
 		if i % 2 == 0:
@@ -22,11 +22,11 @@ if __name__ == '__main__':
 	fp.close()
 
 
-	sample = sys.argv[1]
-	dir = sys.argv[2]
+	sample = sys.argv[2]
+	dir = sys.argv[3]
 	sl = 10000
 	gi = 0
-	fp_w = open("hg38_" + sample + str(sl) + ".fa", 'w')
+	fp_w = open("hg38_" + sample + ".fa", 'w')
 
 	for fn in os.listdir(dir):
 		if sample in fn and 'png' not in fn:
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 			for line in fp:
 				s = line.strip().split('\t')
 				if i > 0:
-					if s[-1] == 'N/A' and int(s[6]) == 0 and float(s[5]) > float(sys.argv[3]):
+					if s[-1] == 'N/A' and int(s[6]) == 0 and float(s[5]) > float(sys.argv[4]):
 						fp_w.write(">g%d %s,%s,%s,%s,%s\n" %(gi, s[0], s[1], s[2], s[3], s[4]))
 						if s[4][0] == '+' and s[4][1] == '-':
 							g1 = genomes[s[0]][int(s[1]) - sl : int(s[1]) + 1]
