4c4d3013e699fc0dbc65480f5ba494d56f1f60df
Suhas Rao <nsuhasrao@gmail.com>
2025-01-21 10:49:02 -0800

    create output dirs if necessary, fix optional args (#28)

--- coral/cli.py
+++ coral/cli.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import os
 import pathlib
 import pickle
 from typing import Annotated
@@ -137,6 +138,7 @@ def reconstruct(
     ] = 1.0,
 ) -> None:
     print(f"Performing reconstruction with options: {ctx.params}")
+    os.makedirs(output_dir, exist_ok=True)
     logging.basicConfig(
         filename=f"{output_dir}/infer_breakpoint_graph.log",
         filemode="w+",
@@ -190,6 +192,7 @@ def cycle_decomposition_mode(
     output_all_path_constraints: OutputPCFlag = False,
     postprocess_greedy_sol: PostProcessFlag = False,
 ) -> None:
+    os.makedirs(output_dir, exist_ok=True)
     bb = infer_breakpoint_graph.LongReadBamToBreakpointMetadata(
         lr_bamfh=pysam.AlignmentFile(str(lr_bam), "rb"),
         lr_graph=[pickle.load(bp_graph)],
@@ -252,26 +255,26 @@ def plot_mode(
     ref: Annotated[str, typer.Option(help="Reference genome.")],
     graph: Annotated[
         typer.FileText | None,
-        typer.Option(help="AmpliconSuite-formatted *.graph file."),
-    ],
-    cycle_file: Annotated[
-        typer.FileText | None,
-        typer.Option(help="AmpliconSuite-formatted cycles file."),
+        typer.Option(help="AmpliconSuite-formatted graph file (*_graph.txt)."),
     ],
     bam: BamArg,
     output_prefix: OutputPrefixArg,
+    cycle_file: Annotated[
+        typer.FileText | None,
+        typer.Option(help="AmpliconSuite-formatted cycles file (*_cycles.txt)."),
+    ] = None,
     num_cycles: Annotated[
         int | None, typer.Option(help="Only plot the first NUM_CYCLES cycles.")
-    ],
+    ] = None,
     region: Annotated[
         str | None,
         typer.Option(
             help="Specifically visualize only this region, argument formatted as 'chr1:pos1-pos2'."
         ),
-    ],
+    ] = None,
     plot_graph: Annotated[
         bool, typer.Option(help="Visualize breakpoint graph.")
-    ] = False,
+    ] = True,
     plot_cycles: Annotated[
         bool, typer.Option(help="Visualize (selected) cycles.")
     ] = False,
@@ -310,6 +313,8 @@ def plot_mode(
     ] = False,
 ) -> None:
     print(f"Performing plot mode with options: {ctx.params}")
+    if "/" in output_prefix:
+        os.makedirs(os.path.dirname(output_prefix), exist_ok=True)
     plot_amplicons.plot_amplicons(
         ref,
         bam,
@@ -337,12 +342,12 @@ def cycle2bed_mode(
     ctx: typer.Context,
     cycle_file: Annotated[
         typer.FileText,
-        typer.Option(help="AmpliconSuite-formatted cycles file."),
+        typer.Option(help="AmpliconSuite-formatted cycles file (*_cycles.txt)."),
     ],
     output_file: Annotated[str, typer.Option(help="Output file name.")],
     num_cycles: Annotated[
         int | None, typer.Option(help="Only plot the first NUM_CYCLES cycles.")
-    ],
+    ] = None,
     rotate_to_min: Annotated[
         bool,
         typer.Option(
